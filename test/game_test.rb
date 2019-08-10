3a3e4cfb06c638efdd6394af2e9f13e2b0197d70
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-10 16:56:56 +0200

    Make test names conform to spec format

--- test/game_test.rb
+++ test/game_test.rb
@@ -37,17 +37,20 @@ class GameTest < Test::Unit::TestCase
     @expected_output_format = /\A([RGBY ]{6}\n){11}[RGBY ]{6}/m
   end
 
-  def test_expected_state_in_expected_format
+  def test_returns_expected_state_in_expected_format
     assert_match @expected_output_format, @game_state
   end
 
-  def test_main_game_output_format
+  def test_returns_expected_game_output_format
     assert_match @expected_output_format, puzzle_fighter(@instructions)
   end
 
-  def test_main_game_loop
+  def test_returns_expected_game_state
     pend("Not implemented")
 
     assert_equal @game_state, puzzle_fighter(@instructions)
+  end
+
+  def test_passes_initial_state_in_expected_format
   end
 end
