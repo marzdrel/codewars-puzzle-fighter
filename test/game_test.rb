e4f669516b66b585b572f342e06f5c82f3e99a0e
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-10 16:47:04 +0200

    Add test to verify format of provided output

--- test/game_test.rb
+++ test/game_test.rb
@@ -33,13 +33,16 @@ class GameTest < Test::Unit::TestCase
       " R  YR",
       "RR  RB"
     ].join("\n")
+
+    @expected_output_format = /\A([RGBY ]{6}\n){11}[RGBY ]{6}/m
+  end
+
+  def test_expected_state_in_expected_format
+    assert_match @expected_output_format, @game_state
   end
 
   def test_main_game_output_format
-    assert_match(
-      /\A([RGBY ]{6}\n){11}[RGBY ]{6}/m,
-      puzzle_fighter(@instructions),
-    )
+    assert_match @expected_output_format, puzzle_fighter(@instructions)
   end
 
   def test_main_game_loop
