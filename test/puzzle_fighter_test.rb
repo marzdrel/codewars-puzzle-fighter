adb0f97edd39f7fff63b27668ee1a116e5f89007
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-10 18:42:32 +0200

    Extract basic PuzzleFighter tests

---
+++ test/puzzle_fighter_test.rb
@@ -0,0 +1,21 @@
+require "test/unit"
+require_relative "../src/app.rb"
+
+class PuzzleFighterTest < Test::Unit::TestCase
+  def setup
+    @expected_output_format = /\A([RGBY ]{6}\n){11}[RGBY ]{6}/m
+    @instructions = Array.new(13, %w[BR LLL])
+  end
+
+  def test_returns_expected_game_output_format
+    assert_match @expected_output_format, puzzle_fighter(@instructions)
+  end
+
+  def test_passes_initial_state_in_expected_format
+    initial_state = Array.new(12, " " * 6)
+    stub(MainLoop).call(initial_state, %w[BR LLL]).times(1)
+    stub(MainLoop).call(anything, anything).times(12) { initial_state }
+
+    PuzzleFighter.call(@instructions)
+  end
+end
