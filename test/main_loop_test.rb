94aa662a4436c75c2bbb9f99bc10a02bdf9a6fbf
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-11 22:37:57 +0200

    Add new test to find mutation of internal state

--- test/main_loop_test.rb
+++ test/main_loop_test.rb
@@ -8,4 +8,14 @@ class MainLoopTest < BaseTestCase
       [*Array.new(10, " " * 6), "B     ", "R     "]
     )
   end
+
+  def test_returns_the_expected_board_layout_with_markers_2
+    state = MainLoop.call([], ["BR", "LLL"])
+    state = MainLoop.call(state, ["YB", "LL"])
+
+    assert_equal(
+      _format(state),
+      [*Array.new(10, " " * 6), "BY    ", "RB    "]
+    )
+  end
 end
