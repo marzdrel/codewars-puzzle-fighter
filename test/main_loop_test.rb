d1b68689db4a052e33c85337fce62a81b6401e8b
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-11 18:10:11 +0200

    Change output formatting in MainLoop test

--- test/main_loop_test.rb
+++ test/main_loop_test.rb
@@ -2,15 +2,17 @@ require "test/unit"
 require_relative "../src/app.rb"
 
 class MainLoopTest < Test::Unit::TestCase
+  def _format(blocks)
+    FormatOutput.call(blocks, 12)
+  end
+
   def setup
     @initial_state = Array.new(12, "      ")
   end
 
   def test_returns_the_expected_board_layout_with_markers
-    pend
-
     assert_match(
-      MainLoop.call(@initial_state, %w[BR LLL]),
+      _format(MainLoop.call(@initial_state, %w[BR LLL])),
       [*Array.new(10, " " * 6), "B     ", "R     "]
     )
   end
