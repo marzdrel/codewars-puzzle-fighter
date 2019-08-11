3cd5c1eafefb65141b65f8a57ea14f954ce4e98b
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-11 17:59:22 +0200

    Change test format to InitialRotator refactor
    InitialRotator now returns object instead of strings for easier
    processing. Strings are more readable in specs, so _format helper will
    conver objects to their strings representation.

--- test/initial_rotator_test.rb
+++ test/initial_rotator_test.rb
@@ -2,6 +2,12 @@ require "test/unit"
 require_relative "../src/app.rb"
 
 class InitialRotatorTest < Test::Unit::TestCase
+  def _format(blocks)
+    blocks.reduce(["      ", "      "]) do |template, block|
+      block.draw_on(template)
+    end
+  end
+
   def test_raises_error_on_invalid_move
     assert_raise ArgumentError do
       InitialRotator.call(["BR", "LLLX"])
@@ -10,147 +16,147 @@ class InitialRotatorTest < Test::Unit::TestCase
 
   def test_moves_the_block_to_initial_location_1
     assert_equal(
-      InitialRotator.call(["BR", "LLL"]),
+      _format(InitialRotator.call(["BR", "LLL"])),
       ["B     ", "R     "],
     )
   end
 
   def test_moves_the_block_to_initial_location_2
     assert_equal(
-      InitialRotator.call(["BG", "ALL"]),
+      _format(InitialRotator.call(["BG", "ALL"])),
       ["GB    ", "      "],
     )
   end
 
   def test_moves_the_block_to_initial_location_3
     assert_equal(
-      InitialRotator.call(["BY", "BRR"]),
+      _format(InitialRotator.call(["BY", "BRR"])),
       ["    BY", "      "],
     )
   end
 
   def test_moves_the_block_to_initial_location_4
     assert_equal(
-      InitialRotator.call(["BB", "AALLL"]),
+      _format(InitialRotator.call(["BB", "AALLL"])),
       ["B     ", "B     "],
     )
   end
 
   def test_moves_the_block_to_initial_location_5
     assert_equal(
-      InitialRotator.call(["YR", ""]),
+      _format(InitialRotator.call(["YR", ""])),
       ["   Y  ", "   R  "],
     )
   end
 
   def test_moves_the_block_to_initial_location_6
     assert_equal(
-      InitialRotator.call(["BG", "BBRR"]),
+      _format(InitialRotator.call(["BG", "BBRR"])),
       ["     G", "     B"],
     )
   end
 
   def test_moves_the_block_to_initial_location_7
     assert_equal(
-      InitialRotator.call(["RR", "BBBBLLL"]),
+      _format(InitialRotator.call(["RR", "BBBBLLL"])),
       ["R     ", "R     "],
     )
   end
 
   def test_moves_the_block_to_initial_location_8
     assert_equal(
-      InitialRotator.call(["YG", "BBBALL"]),
+      _format(InitialRotator.call(["YG", "BBBALL"])),
       [" G    ", " Y    "],
     )
   end
 
   def test_moves_the_block_to_initial_location_9
     assert_equal(
-      InitialRotator.call(["BR", "L"]),
+      _format(InitialRotator.call(["BR", "L"])),
       ["  B   ", "  R   "],
     )
   end
 
   def test_moves_the_block_to_initial_location_10
     assert_equal(
-      InitialRotator.call(["BR", "LLLLL"]),
+      _format(InitialRotator.call(["BR", "LLLLL"])),
       ["B     ", "R     "],
     )
   end
 
   def test_moves_the_block_to_initial_location_11
     assert_equal(
-      InitialRotator.call(["BR", "R"]),
+      _format(InitialRotator.call(["BR", "R"])),
       ["    B ", "    R "],
     )
   end
 
   def test_moves_the_block_to_initial_location_12
     assert_equal(
-      InitialRotator.call(["BR", "RRRRRR"]),
+      _format(InitialRotator.call(["BR", "RRRRRR"])),
       ["     B", "     R"],
     )
   end
 
   def test_moves_the_block_to_initial_location_13
     assert_equal(
-      InitialRotator.call(["BR", "LLLLLLRRRRR"]),
+      _format(InitialRotator.call(["BR", "LLLLLLRRRRR"])),
       ["     B", "     R"],
     )
   end
 
   def test_moves_the_block_to_initial_location_14
     assert_equal(
-      InitialRotator.call(["BR", "LRLRLRLRLR"]),
+      _format(InitialRotator.call(["BR", "LRLRLRLRLR"])),
       ["   B  ", "   R  "],
     )
   end
 
   def test_rotates_the_block_clockwise_1
     assert_equal(
-      InitialRotator.call(["BR", "A"]),
+      _format(InitialRotator.call(["BR", "A"])),
       ["  RB  ", "      "],
     )
   end
 
   def test_rotates_the_block_clockwise_2
     assert_equal(
-      InitialRotator.call(["BR", "AA"]),
+      _format(InitialRotator.call(["BR", "AA"])),
       ["   R  ", "   B  "],
     )
   end
 
   def test_rotates_the_block_clockwise_3
     assert_equal(
-      InitialRotator.call(["BR", "AAA"]),
+      _format(InitialRotator.call(["BR", "AAA"])),
       ["   BR ", "      "],
     )
   end
 
   def test_rotates_the_block_clockwise_4
     assert_equal(
-      InitialRotator.call(["BR", "AAAA"]),
+      _format(InitialRotator.call(["BR", "AAAA"])),
       ["   B  ", "   R  "],
     )
   end
 
   def test_rotates_the_block_clockwise_5
     assert_equal(
-      InitialRotator.call(["BR", "AAAAAAA"]),
+      _format(InitialRotator.call(["BR", "AAAAAAA"])),
       ["   BR ", "      "],
     )
   end
 
   def test_rotates_the_block_clockwise_6
     assert_equal(
-      InitialRotator.call(["BR", "AAAAAAAA"]),
+      _format(InitialRotator.call(["BR", "AAAAAAAA"])),
       ["   B  ", "   R  "],
     )
   end
 
   def test_rotates_the_block_anticlockwise_1
     assert_equal(
-      InitialRotator.call(["BR", "B"]),
+      _format(InitialRotator.call(["BR", "B"])),
       ["   BR ", "      "],
     )
   end
