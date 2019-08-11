df59416ee32c1e3a079b3f78f4ec835674772057
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-11 22:47:25 +0200

    Change api for Block in test

--- test/board_test.rb
+++ test/board_test.rb
@@ -3,15 +3,10 @@ require_relative "../src/app.rb"
 
 class BoardTest < Test::Unit::TestCase
   def test_inserts_new_blocks_into_the_board
-    board = Board.new [
-      Block.new("A", 1, 11),
-      Block.new("A", 1, 10),
-    ]
-
-    blocks = board.insert [
-      Block.new("Y", 1, 0),
-      Block.new("B", 2, 0),
-    ]
+    blocks = Board.call(
+      [Block.new("A", 1, 11), Block.new("A", 1, 10)],
+      [Block.new("Y", 1, 0), Block.new("B", 2, 0)],
+    )
 
     assert_equal(
       blocks,
