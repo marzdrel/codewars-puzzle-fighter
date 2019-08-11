8ddc077d9fce2b276bd9fe81584d84e2e0a62692
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-11 11:56:26 +0200

    Add test covering invalid move input

--- test/initial_rotator_test.rb
+++ test/initial_rotator_test.rb
@@ -2,6 +2,12 @@ require "test/unit"
 require_relative "../src/app.rb"
 
 class InitialRotatorTest < Test::Unit::TestCase
+  def test_raises_error_on_invalid_move
+    assert_raise ArgumentError do
+      InitialRotator.call(["BR", "LLLX"])
+    end
+  end
+
   def test_moves_the_piece_to_initial_location_1
     pend
 
