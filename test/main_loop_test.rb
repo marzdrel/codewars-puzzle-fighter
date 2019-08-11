91c1f36cc4a0b15d19a0a7d32510439c4b0c79ef
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-11 19:03:47 +0200

    Refactor MainLoop test

--- test/main_loop_test.rb
+++ test/main_loop_test.rb
@@ -2,13 +2,9 @@ require "test/unit"
 require_relative "../src/app.rb"
 
 class MainLoopTest < BaseTestCase
-  def setup
-    @initial_state = Array.new(12, "      ")
-  end
-
   def test_returns_the_expected_board_layout_with_markers
-    assert_match(
-      _format(MainLoop.call(@initial_state, %w[BR LLL])),
+    assert_equal(
+      _format(MainLoop.call([], %w[BR LLL])),
       [*Array.new(10, " " * 6), "B     ", "R     "]
     )
   end
