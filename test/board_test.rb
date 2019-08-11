c073400303fbcc4039e919a12e525ab7b1db2c08
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-11 22:48:23 +0200

    Improve clarity in test data for Board

--- test/board_test.rb
+++ test/board_test.rb
@@ -4,8 +4,14 @@ require_relative "../src/app.rb"
 class BoardTest < Test::Unit::TestCase
   def test_inserts_new_blocks_into_the_board
     blocks = Board.call(
-      [Block.new("A", 1, 11), Block.new("A", 1, 10)],
-      [Block.new("Y", 1, 0), Block.new("B", 2, 0)],
+      [
+        Block.new("A", 1, 11),
+        Block.new("A", 1, 10),
+      ],
+      [
+        Block.new("Y", 1, 0),
+        Block.new("B", 2, 0),
+      ],
     )
 
     assert_equal(
