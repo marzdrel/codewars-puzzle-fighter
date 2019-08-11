25c06ea91524787c519cb053de8b61feba0105a1
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-11 19:08:52 +0200

    Add basic Board test with block insertion

--- test/board_test.rb
+++ test/board_test.rb
@@ -3,5 +3,24 @@ require_relative "../src/app.rb"
 
 class BoardTest < Test::Unit::TestCase
   def test_inserts_new_blocks_into_the_board
+    board = Board.new [
+      Block.new("A", 1, 11),
+      Block.new("A", 1, 10),
+    ]
+
+    blocks = board.insert [
+      Block.new("Y", 1, 0),
+      Block.new("B", 2, 0),
+    ]
+
+    assert_equal(
+      blocks,
+      [
+        Block.new("A", 1, 11),
+        Block.new("A", 1, 10),
+        Block.new("B", 2, 11),
+        Block.new("Y", 1, 9),
+      ]
+    )
   end
 end
