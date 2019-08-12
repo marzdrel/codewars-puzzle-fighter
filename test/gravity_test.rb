823125eeff9c8190228ca492ddc221b0849e7545
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-12 22:02:41 +0200

    Add basic Gravity test

---
+++ test/gravity_test.rb
@@ -0,0 +1,25 @@
+require "test/unit"
+require_relative "../src/app.rb"
+
+class GravityTest < BaseTestCase
+  def test_moves_basic_games_down
+    templates = [
+      "     G", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "YYY   ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "R     ", "     G",
+      "     Y", "     Y",
+      "  R  Y", "Y Y  Y",
+      "   YBY", "RYRYBY",
+    ]
+
+    input, expected = _set_from_ascii(templates)
+
+    assert_equal expected, Gravity.call(input)
+  end
+end
