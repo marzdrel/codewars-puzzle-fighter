4e832b4dbc7dcaca88b5e2d9b964ddbc5c2f21fa
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-12 21:57:44 +0200

    Move ascii-input test helper to base test class

--- test/base_test_case.rb
+++ test/base_test_case.rb
@@ -5,4 +5,21 @@ class BaseTestCase < Test::Unit::TestCase
   def _format(blocks, rows = 12)
     FormatOutput.call(blocks, rows)
   end
+
+  def _set_from_ascii(templates)
+    convert_board = proc do |board|
+      board.flat_map.with_index(0) do |row, y|
+        row.chars.flat_map.with_index(0) do |kind, x|
+          next [] if kind == " "
+
+          Block.new(kind, x, y)
+        end
+      end
+    end
+
+    templates
+      .partition
+      .with_index(1) { |_, i| i.odd? }
+      .map(&convert_board)
+  end
 end
--- test/crash_test.rb
+++ test/crash_test.rb
@@ -1,24 +1,7 @@
 require "test/unit"
 require_relative "../src/app.rb"
 
-class CrashTest < Test::Unit::TestCase
-  def _set_from_ascii(templates)
-    convert_board = proc do |board|
-      board.flat_map.with_index(0) do |row, y|
-        row.chars.flat_map.with_index(0) do |kind, x|
-          next [] if kind == " "
-
-          Block.new(kind, x, y)
-        end
-      end
-    end
-
-    templates
-      .partition
-      .with_index(1) { |_, i| i.odd? }
-      .map(&convert_board)
-  end
-
+class CrashTest < BaseTestCase
   def test_removes_crash_gems_from_board
     templates = [
       "      ", "      ",
