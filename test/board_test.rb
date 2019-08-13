f3e8a2da2133c37fcb7b8b8440b7e1f0c32fa9aa
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-13 17:12:49 +0200

    Add PowerBlock example to Board test

--- test/board_test.rb
+++ test/board_test.rb
@@ -30,4 +30,33 @@ class BoardTest < BaseTestCase
 
     assert_equal expected.sort, blocks.sort
   end
+
+  def test_inserts_new_blocks_into_the_board_with_power_blocks
+    templates = [
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", " YB   ",
+      "RRRR Y", "RRRR Y",
+      "RRRR Y", "RRRR Y",
+      "   BBY", "   BBY",
+    ]
+
+    input, expected = _set_from_ascii(templates)
+
+    blocks = Board.call(
+      input,
+      [
+        Block.new("Y", 1, 0),
+        Block.new("B", 2, 0),
+      ],
+    )
+
+    assert_equal expected.sort, blocks.sort
+  end
 end
