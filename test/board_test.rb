89cb226da0a35b44b1278f20c71d3efc10ad080a
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-13 17:42:48 +0200

    Add empty tests for hanging logic

--- test/board_test.rb
+++ test/board_test.rb
@@ -2,4 +2,11 @@ require "test/unit"
 require_relative "../src/app.rb"
 
 class BoardTest < BaseTestCase
+  def test_is_hanging_over_board
+    pend
+  end
+
+  def test_returns_all_hanging_blocks
+    pend
+  end
 end
