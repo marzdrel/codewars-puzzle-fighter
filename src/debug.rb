51658e4837f1f5ef405e55d42e58e35b4b5e71f7
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-14 23:50:11 +0200

    Add more colors to Debug

--- src/debug.rb
+++ src/debug.rb
@@ -1,9 +1,5 @@
 module Colors
   refine String do
-    def black
-      "\e[30m#{self}\e[0m"
-    end
-
     def bg_red
       "\e[41m#{self}\e[0m"
     end
@@ -20,8 +16,36 @@ module Colors
       "\e[44m#{self}\e[0m"
     end
 
+    def black
+      "\e[30m#{self}\e[0m"
+    end
+
     def red
       "\e[31m#{self}\e[0m"
+    end
+
+    def green
+      "\e[32m#{self}\e[0m"
+    end
+
+    def brown
+      "\e[33m#{self}\e[0m"
+    end
+
+    def blue
+      "\e[34m#{self}\e[0m"
+    end
+
+    def magenta
+      "\e[35m#{self}\e[0m"
+    end
+
+    def cyan
+      "\e[36m#{self}\e[0m"
+    end
+
+    def gray
+      "\e[37m#{self}\e[0m"
     end
   end
 end
