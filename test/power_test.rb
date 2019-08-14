7e5aab594e9e0f5c8b0df2ec27fb24f90e4d2805
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-14 15:11:14 +0200

    Add tests for Power Block marking

--- test/power_test.rb
+++ test/power_test.rb
@@ -12,16 +12,64 @@ class PowerTest < BaseTestCase
       "      ", "      ",
       "     g", "      ",
       "    RR", "      ",
-      "RY  RR", "RY    ",
-      "BBRgBY", "BBRgBY",
-      "BBRYYY", "BRRRRY",
-      "BBBYYB", "BRBYBY",
+      "RY  RR", "      ",
+      "BBRgBY", "      ",
+      "BBBYBB", "BBB   ",
+      "BBBYBB", "BBB   ",
     ]
 
     input, expected = _set_from_ascii(templates)
 
     ascii_expected, ascii_output =
-      _format_all(expected.sort, Power.call(input).sort)
+      _format_all(expected.sort, Power.call(input, "B").sort)
+
+    assert_equal ascii_expected, ascii_output
+  end
+
+  def test_finds_nothing_if_there_is_no_gems
+    templates = [
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "     g", "      ",
+      "    RR", "      ",
+      "RY  RR", "      ",
+      "BBRgBY", "      ",
+      "BYBYYB", "      ",
+      "BBBYBB", "      ",
+    ]
+
+    input, expected = _set_from_ascii(templates)
+
+    ascii_expected, ascii_output =
+      _format_all(expected.sort, Power.call(input, "G").sort)
+
+    assert_equal ascii_expected, ascii_output
+  end
+
+  def test_finds_only_power_blocks_of_size_4_or_more
+    templates = [
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "     g", "      ",
+      "    RR", "      ",
+      "RY  RR", "      ",
+      "BBRgBB", "      ",
+      "BrBYBr", "      ",
+      "BBBYBB", "      ",
+    ]
+
+    input, expected = _set_from_ascii(templates)
+
+    ascii_expected, ascii_output =
+      _format_all(expected.sort, Power.call(input, "B").sort)
 
     assert_equal ascii_expected, ascii_output
   end
