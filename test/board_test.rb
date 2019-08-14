90dabaf56180b3c8f0d3c9ff867a9049dd575b9d
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-14 13:48:39 +0200

    Add spec for detecting Power Gems in Board

--- test/board_test.rb
+++ test/board_test.rb
@@ -9,4 +9,30 @@ class BoardTest < BaseTestCase
   def test_returns_all_hanging_blocks
     pend
   end
+
+  def test_is_memer_of_power_block
+    pend
+
+    template = [
+      "     Y",
+      "      ",
+      "      ",
+      "      ",
+      "  B   ",
+      " Y    ",
+      "      ",
+      "RRRR  ",
+      "RRRR Y",
+      " G B  ",
+      "   BBY",
+      "   RBR",
+    ]
+
+    board = Board.new(_from_ascii(template))
+
+    assert board.locked_power_block?(board.at(0, 8))
+    assert board.locked_power_block?(board.at(0, 7))
+    assert board.locked_power_block?(board.at(3, 8))
+    refute board.locked_power_block?(board.at(4, 0))
+  end
 end
