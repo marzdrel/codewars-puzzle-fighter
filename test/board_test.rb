60f040cdb1d6361721ff6be15cd9554effd1271a
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-14 15:20:59 +0200

    Add test for static Power Block on Board

--- test/board_test.rb
+++ test/board_test.rb
@@ -2,37 +2,32 @@ require "test/unit"
 require_relative "../src/app.rb"
 
 class BoardTest < BaseTestCase
+  def test_is_truthy_for_hanging_power_block
+    templates = [
+      "     Y", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "  B   ", "      ",
+      " Y    ", "      ",
+      "      ", "      ",
+      "RRRR  ", "RRRR  ",
+      "RRRR Y", "RRRR  ",
+      " G B  ", "      ",
+      "   BBY", "      ",
+      "   BBB", "      ",
+    ]
+
+    input, power_block  = _set_from_ascii(templates)
+
+    assert Board.new(input).power_block_static?(power_block)
+  end
+
   def test_is_hanging_over_board
     pend
   end
 
   def test_returns_all_hanging_blocks
     pend
-  end
-
-  def test_is_memer_of_power_block
-    pend
-
-    template = [
-      "     Y",
-      "      ",
-      "      ",
-      "      ",
-      "  B   ",
-      " Y    ",
-      "      ",
-      "RRRR  ",
-      "RRRR Y",
-      " G B  ",
-      "   BBY",
-      "   RBR",
-    ]
-
-    board = Board.new(_from_ascii(template))
-
-    assert board.locked_power_block?(board.at(0, 8))
-    assert board.locked_power_block?(board.at(0, 7))
-    assert board.locked_power_block?(board.at(3, 8))
-    refute board.locked_power_block?(board.at(4, 0))
   end
 end
