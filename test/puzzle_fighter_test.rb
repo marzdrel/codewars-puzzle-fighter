1596bd18c1da1fc5356f4080870b3f78468dbb08
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-16 00:25:24 +0200

    Change main PuzzleFighter test

--- test/puzzle_fighter_test.rb
+++ test/puzzle_fighter_test.rb
@@ -4,7 +4,7 @@ require_relative "../src/app.rb"
 class PuzzleFighterTest < Test::Unit::TestCase
   def setup
     @expected_output_format = /\A([RGBY ]{6}\n){11}[RGBY ]{6}/m
-    @instructions = Array.new(13, %w[BR ALL])
+    @instructions = Array.new(11, %w[BR ALL])
   end
 
   def test_returns_expected_game_output_format
@@ -13,7 +13,7 @@ class PuzzleFighterTest < Test::Unit::TestCase
 
   def test_passes_initial_state_in_expected_format
     stub(MainLoop).call([], %w[BR ALL]).times(1) { [] }
-    stub(MainLoop).call(anything, anything).times(12) { [] }
+    stub(MainLoop).call(anything, anything).times(10) { [] }
 
     PuzzleFighter.call(@instructions)
   end
