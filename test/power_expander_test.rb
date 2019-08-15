0b3d1c46e8410c7299221e7cec41c4ca532cbea4
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-15 16:36:24 +0200

    Add PowerExpander Test

---
+++ test/power_expander_test.rb
@@ -0,0 +1,64 @@
+require "test/unit"
+require_relative "../src/app.rb"
+
+class PowerExpanderTest < BaseTestCase
+  using ObjectExtensions
+
+  def test_expands_the_power_block
+    input, expected = _set_from_ascii [
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "  R   ", "RR    ",
+      "RRB   ", "RR    ",
+      "RRB   ", "RR    ",
+      "YYYYYY", "      ",
+    ]
+
+    result =
+      input
+      .then(&PowerCombiner)
+      .then(&Board)
+      .then(["RR", "BLL"], &MainLoop)
+      .then(&PowerCombiner)
+      .then(&PowerMerger)
+      .then(&PowerExpander)
+      .then(&:power_blocks)
+      .then(&:first)
+
+    assert_equal(*_format_all(expected, result))
+  end
+
+  def test_expands_properly
+    input, expected = _set_from_ascii [
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "R     ", "      ",
+      "R R   ", "RRR   ",
+      "RRR   ", "RRR   ",
+      "RRR   ", "RRR   ",
+      "RRR   ", "RRR   ",
+    ]
+
+    result =
+      input
+      .then(&PowerCombiner)
+      .then(&Board)
+      .then(["RR", "ALL"], &MainLoop)
+      .then(&PowerExpander)
+      .then(&:power_blocks)
+      .then(&:first)
+
+    assert_equal(*_format_all(expected, result))
+  end
+end
