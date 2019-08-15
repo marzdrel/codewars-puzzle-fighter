9b6f42395c8b4a07c559ad3d23d8ccd2d927f9fd
Marz Drel <marzdrel@users.noreply.github.com>
2019-08-15 22:53:50 +0200

    Add tests for Rainbow blocks

--- test/rainbow_test.rb
+++ test/rainbow_test.rb
@@ -14,7 +14,7 @@ class RainbowTest < BaseTestCase
       "      ", "      ",
       "R     ", "R     ",
       "BB   Y", "BB    ",
-      "BBR  Y", "BBR   ",
+      "BBR  Y", "BBR R ",
       "BBBYBY", "BBB B ",
     ]
 
@@ -23,6 +23,32 @@ class RainbowTest < BaseTestCase
     result =
       input
       .then(["0R", "A"], &MainLoop)
+      .then(&Rainbow)
+
+    assert_equal(*_format_all(expected, result))
+  end
+
+  def test_lost_rainbow_does_nothing
+    templates = [
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "      ", "      ",
+      "R     ", "R     ",
+      "BB   Y", "BB   Y",
+      "BBR  Y", "BBRR Y",
+      "BBBY Y", "BBBY Y",
+    ]
+
+    input, expected = _set_from_ascii(templates)
+
+    result =
+      input
+      .then(["0R", "BR"], &MainLoop)
       .then(&Rainbow)
 
     assert_equal(*_format_all(expected, result))
